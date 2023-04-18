385f559e654ff75c46c18c7ecbc042fe7279fdac
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-04-18 23:10:47 +0300

    Initial Commit

---
+++ TaskManagementCoreDataSwiftUI/TaskManagementCoreDataSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskManagementCoreDataSwiftUIApp.swift
+//  TaskManagementCoreDataSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementCoreDataSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
